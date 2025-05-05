cfbe5ba4d4ff74b72911737b5c04e00a8e7216be
charmeskoh <kohshunshin775@gmail.com>
2025-05-05 20:48:44 +0800

    added strchr, strnstr and strrchr

---
+++ ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shkoh <shkoh@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 19:52:38 by shkoh             #+#    #+#             */
+/*   Updated: 2025/05/05 20:24:40 by shkoh            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && (i + j) < len)
+		{
+			j++;
+			if (needle[j] == '\0')
+				return ((char *)(haystack + i));
+		}
+		i++;
+	}
+	return (NULL);
+}
