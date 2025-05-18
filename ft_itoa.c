5d4d9fb178d17764c10154ccb39a420696621692
charmeskoh <kohshunshin775@gmail.com>
2025-05-18 22:31:10 +0800

    finally did itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,63 @@
 /*   By: shkoh <shkoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 15:05:32 by shkoh             #+#    #+#             */
-/*   Updated: 2025/05/07 15:10:33 by shkoh            ###   ########.fr       */
+/*   Updated: 2025/05/18 22:30:27 by shkoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_intlen(long n)
+{
+	int	count;
+
+	count = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+	{
+		count++;
+		n = -n;
+	}
+	while (n != 0)
+	{
+		count++;
+		n = n / 10;
+	}
+	return (count);
+}
+
+void	ft_digits(char *str, long n, int len)
+{
+	while (n != 0)
+	{
+		str[--len] = (n % 10) + '0';
+		n /= 10;
+	}
+}
+
 char	*ft_itoa(int nbr)
 {
+	char	*str;
+	long	n;
+	int		len;
 
+	n = nbr;
+	len = ft_intlen(n);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (n == 0)
+	{
+		str[0] = '0';
+		return (str);
+	}
+	if (n < 0)
+	{
+		n = -n;
+		str[0] = '-';
+	}
+	ft_digits(str, n, len);
+	return (str);
 }
