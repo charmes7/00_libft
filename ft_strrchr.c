aca5f6bf30828556399ab0d8b247290c10a4b149
charmeskoh <kohshunshin775@gmail.com>
2025-05-26 05:56:11 +0800

    final update

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: shkoh <shkoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 19:52:39 by shkoh             #+#    #+#             */
-/*   Updated: 2025/05/11 19:32:49 by shkoh            ###   ########.fr       */
+/*   Updated: 2025/05/26 05:35:30 by shkoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 char	*ft_strrchr(const char *str, int c)
 {
-	char	*ptr;
 	int		i;
 
-	ptr = NULL;
-	i = 0;
-	while (str[i])
+	if ((unsigned char)c == '\0')
+		return ((char *)str + ft_strlen(str));
+	i = ft_strlen(str) - 1;
+	while (i >= 0)
 	{
 		if (str[i] == (unsigned char)c)
-			ptr = (char *)&str[i];
-		i++;
+			return ((char *)str + i);
+		i--;
 	}
-	if ((unsigned char)c == '\0')
-		ptr = (char *)&str[i];
-	return (ptr);
+	return (NULL);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: shkoh <shkoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 15:10:49 by shkoh             #+#    #+#             */
-/*   Updated: 2025/05/24 22:16:53 by shkoh            ###   ########.fr       */
+/*   Updated: 2025/05/26 05:52:06 by shkoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <string.h>
-# include <stdint.h>
 # include <limits.h>
 
 typedef struct s_list
@@ -25,10 +23,10 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
+/* Mandatory */
 void	*ft_memset(void *ptr, int value, size_t num);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
@@ -43,21 +41,15 @@ int		ft_isprint(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 char	**ft_split(char const *s, char c);
-char	*ft_strcat(char *dest, char *src);
 char	*ft_strchr(const char *str, int c);
-int		ft_strcmp(char *s1, char*s2);
-char	*ft_strcpy(char *dest, char *src);
 char	*ft_strdup(char *src);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *str);
-char	*ft_strncat(char *dest, char *src, unsigned int nb);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strncpy(char *dest, char *src, unsigned int n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *str, int c);
-char	*ft_strstr(char *str, char *to_find);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_substr(char const *s, unsigned int start, size_t len);
